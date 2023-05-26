ba7e828a7fc3babc25eed8246be5c56c1dad979c
AFPedreros <felipe.pedreros94@gmail.com>
2023-05-26 09:33:15 -0500

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,27 @@ import { buttonVariants } from "@/components/ui/button"
 import { Product } from "@/components/product"
 
 export default async function IndexPage() {
-  // const products = await getProducts()
+  const products = await getProducts()
 
-  // console.log(products)
+  console.log(products)
 
   return (
-    <section className="container grid items-center gap-6 pt-6 pb-8 md:py-10">
-      <Product
-        product={{
-          name: "Hosting",
-          id: 1,
-          description: "Descripcion del producto 1",
-          price: 1000,
-        }}
-        className="w-[250px]"
-        aspectRatio="portrait"
-        width={250}
-        height={330}
-      />
+    <section className="container grid items-center justify-center gap-6 pt-6 pb-8 md:grid-cols-2 lg:grid-cols-4 md:py-10">
+      {products.map((product) => (
+        <Product
+          key={product.id}
+          product={{
+            name: product.fields.Nombre,
+            id: product.fields.Id,
+            description: product.fields.Descripción,
+            price: product.fields.Precio,
+          }}
+          className="w-[250px]"
+          aspectRatio="square"
+          width={250}
+          height={330}
+        />
+      ))}
     </section>
   )
 }
