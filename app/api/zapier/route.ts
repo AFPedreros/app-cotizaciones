4b32800d030b5c3c2f7da888493eeca52b0fbf3a
AFPedreros <felipe.pedreros94@gmail.com>
2023-06-01 21:38:18 -0500

    commit

--- app/api/zapier/route.ts
+++ app/api/zapier/route.ts
@@ -1,19 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next"
-import { mutate } from "swr"
-
-import { Product } from "@/lib/types"
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    // Zapier enviará la información actualizada del producto en el cuerpo de la solicitud
-    const updatedProduct = req.body
-
-    // Actualiza los datos en SWR
-    console.log(updatedProduct)
-
     // Responde con un estado de éxito
     res.status(200).json({ status: "success" })
   } else {
